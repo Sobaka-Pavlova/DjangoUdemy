49035bf18df0b76ac3be0991a31a465309679e1d
Sokol <sergei.proverka@mail.ru>
2020-05-26 03:13:21 -0400

    Added filter by realtor in listings

--- listings/admin.py
+++ listings/admin.py
@@ -5,4 +5,6 @@ from .models import Listing
 class ListingAdmin(admin.ModelAdmin):
     list_display= ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
     list_display_links= ('id', 'title')
+    list_filter= ('realtor',)
+
 admin.site.register(Listing, ListingAdmin)
