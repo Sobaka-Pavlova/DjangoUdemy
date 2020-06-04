fbae2e4d69db62a84a4030c993d16ea8de2a5dd9
Sokol <sergei.proverka@mail.ru>
2020-06-03 20:06:22 -0400

    Fixed numbers in paginator

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
