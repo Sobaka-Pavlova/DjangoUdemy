680a660a9856b4ff584bf86f646135a108b2c426
Sokol <sergei.proverka@mail.ru>
2020-06-07 19:27:07 -0400

    Dynamic page titles, logout and navbar auth links

--- btre/settings.py
+++ btre/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
     }
 }
